8431fb136ebad44be4c1e7a171f402cf9cf02554
harshsimform <harsh.v@simformsolutions.com>
2023-05-29 14:08:27 +0530

    products api created

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.use(express.json());
+app.use(cors());
+
+const productsRouter = require("./routes/products");
+app.use("/product", productsRouter);
+
+app.listen(3000, () => console.log("Server Started"));
---
+++ models/product.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  discountedPrice: {
+    type: Number,
+    required: true,
+  },
+  originalPrice: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  recordDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
